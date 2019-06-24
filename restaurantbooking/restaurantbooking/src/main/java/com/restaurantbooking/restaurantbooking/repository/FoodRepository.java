d6c45d67842b09137687ab86bb291614d47bd330
Vishalini J <Vishalini.J@mrcooper.com>
2019-06-24 11:30:09 +0530

    resolved work on the project

--- restaurantbooking/restaurantbooking/src/main/java/com/restaurantbooking/restaurantbooking/repository/FoodRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurantbooking.restaurantbooking.repository;
-
-import com.restaurantbooking.restaurantbooking.model.Food;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FoodRepository extends CrudRepository<Food,String> {
-}
